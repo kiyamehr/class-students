642ad9abccff98aa9fb4faedbceaa69f698ba16d
Kiamehr <kiamehrmoradii@gmail.com>
2025-11-01 16:51:58 +0330

    halfassing the tests

--- students_test.py
+++ students_test.py
@@ -1,6 +1,12 @@
 import unittest
 from students import Students
 
+
+# hall nadaram test benivasam bashe bara bad hahahaha
 class TestStudent(unittest.TestCase):
-    def test_add_score(self):
-        self.assert
+    def test_add_score(self, score):
+        with self.assertRaises(ValueError):
+            return score == int(score)
+        
+if __name__ == "__main__":
+    unittest.main()
